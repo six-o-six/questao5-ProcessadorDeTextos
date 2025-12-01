f6f858e9b691adbc4bf7fbd7ebc8070086cce48b
Jak <jakelynecavalcanti@gmail.com>
2025-12-01 15:23:52 -0300

    Initial commit: Implementar estrutura inicial do projeto

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module questao5_ProcessadorDeTextos {
+}
---
+++ src/processador/TudoMinuscula.java
@@ -0,0 +1,26 @@
+package processador;
+
+public class TudoMinuscula {
+	
+	public static String converterTextoEmMinusculo(String entrada) {
+		char[] caracteres = extrairCaracteres(entrada);
+		char[] caracateresAgoraMinusculos = converterCaracteresEmMinusculo(caracteres);
+		String resultado = juntarCaractaresAgoraMinusculo(caracateresAgoraMinusculos);
+		return resultado;
+	}
+	
+	private static String juntarCaractaresAgoraMinusculo(char[] chars) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private static char[] converterCaracteresEmMinusculo(char[] chars) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private static char[] extrairCaracteres(String texto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
